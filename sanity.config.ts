ba576ad903cd5a2e9fe850fc51f3d8806127c672
Sulaiman Agara <airplaneboy00@gmail.com>
2024-04-30 23:16:54 -0400

    Added sanity MDX integration

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
+import { markdownSchema } from 'sanity-plugin-markdown';
 
 import { apiVersion, dataset, projectId } from './sanity/env';
 import { schema } from './sanity/schema';
@@ -10,5 +11,5 @@ export default defineConfig({
   projectId,
   dataset,
   schema,
-  plugins: [structureTool(), visionTool({ defaultApiVersion: apiVersion })],
+  plugins: [structureTool(), visionTool({ defaultApiVersion: apiVersion }), markdownSchema()],
 });
