37b08930568c1ee4b611008efcc1bef486476d89
Sulaiman Agara <airplaneboy00@gmail.com>
2024-05-05 16:41:49 -0400

    Added padding bottom for consequent pages to adapt to the fixed navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <main className='min-h-[200vh]'>
+    <main className='min-h-[100vh_-_57px] pb-[57px]'>
       <Navbar />
     </main>
   );
