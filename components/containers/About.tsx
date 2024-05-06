e686c6b4938b0e2bdd712f9caaa57e87bb740642
Sulaiman Agara <airplaneboy00@gmail.com>
2024-05-06 18:10:20 -0400

    Added content to about page

--- components/containers/About.tsx
+++ components/containers/About.tsx
@@ -1,5 +1,11 @@
+import HeroSection from './HeroSection';
+
 const About = () => {
-  return <div className='pt-[57px]'>About</div>;
+  return (
+    <div className='pt-[57px] mx-auto max-w-7xl px-10'>
+      <HeroSection />
+    </div>
+  );
 };
 
 export default About;
