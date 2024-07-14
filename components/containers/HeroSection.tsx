7feda4261750efc08e5c73a189e29aa672a02932
Sulaiman Agara <airplaneboy00@gmail.com>
2024-07-14 13:14:11 -0400

    made hero section responsive for tablet

--- components/containers/HeroSection.tsx
+++ components/containers/HeroSection.tsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
       <div className='absolute pointer-events-none inset-0 flex items-center justify-center  bg-[linear-gradient(0deg,rgba(255,255,255,1)_0%,rgba(0,0,0,0)_15%)] dark:bg-[linear-gradient(0deg,rgba(0,0,0,1)_0%,rgba(0,0,0,0)_15%)] z-10'></div>
       <div className='h-full w-full min-w-screen z-0 absolute pointer-events-none left-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]' />
 
-      <div className='section flex flex-row justify-between items-center gap-10'>
+      <div className='section pt-16 lg:pt-0 flex flex-col lg:flex-row justify-between items-center gap-10'>
         {/* <div className='inset-0 absolute w-full h-full bg-[linear-gradient(0deg,_rgba(52,50,44,0.7539390756302521)_0%,_rgba(46,100,97,0)_30%,_rgba(34,193,195,0)_100%)]' /> */}
         <div className={sectionContentStyle}>
           <h1 className='text-5xl leading-[1] font-extrabold  pb-3 min-w-[580px] font-display flex flex-col gap-5'>
@@ -42,8 +42,8 @@ const HeroSection = () => {
         </div>
 
         <div className='relative'>
-          <div className='pointer-events-none absolute w-full bg-gradient-to-r from-transparent via-transparent to-white dark:to-[#070707] z-10 h-full' />
-          <pre className='hidden dark:block pointer-events-none absolute w-full h-full z-[1] bg-[linear-gradient(330deg,rgba(0,0,0,0)_0%,rgba(0,0,0,0)_20%,rgba(255,255,255,1)_20%,rgba(255,255,255,1)_45%,rgba(255,255,255,1)_45%,rgba(255,255,255,1)_55%,rgba(255,255,255,1)_55%,rgba(255,255,255,1)_80%,rgba(0,0,0,0)_80%)]' />
+          <div className='pointer-events-none scale-105 absolute w-full bg-gradient-to-r from-transparent via-transparent to-white dark:to-[#070707] z-10 h-full' />
+          <pre className='hidden dark:block pointer-events-none scale-95 absolute w-full h-full z-[1] bg-[linear-gradient(330deg,rgba(0,0,0,0)_0%,rgba(0,0,0,0)_20%,rgba(255,255,255,1)_20%,rgba(255,255,255,1)_45%,rgba(255,255,255,1)_45%,rgba(255,255,255,1)_55%,rgba(255,255,255,1)_55%,rgba(255,255,255,1)_80%,rgba(0,0,0,0)_80%)]' />
           <ASCIIProfilePicture className='overflow text-[9px] leading-none inset-0 z-[5] relative text-black' />
         </div>
       </div>
