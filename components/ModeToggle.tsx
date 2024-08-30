e211da74a2397b9abbd21d7085e02ee772269c54
Sulaiman Agara <airplaneboy00@gmail.com>
2024-08-30 00:47:03 -0400

    adjusted color of mode toggle

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -19,14 +19,14 @@ export function ModeToggle() {
   return (
     <>
       <DropdownMenu>
-        <DropdownMenuTrigger asChild className='bg-transparent'>
-          <Button variant='outline' size='icon' className='rounded-sm min-w-9 max-w-9 min-h-9 max-h-9'>
+        <DropdownMenuTrigger asChild className='bg-transparent dark:bg-transparent'>
+          <Button variant='outline' size='icon' className='rounded-md min-w-9 max-w-9 min-h-9 max-h-9'>
             <SunIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
             <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
             <span className='sr-only'>Toggle theme</span>
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent align='end' className='bg-white/50 backdrop-blur dark:bg-black dark:backdrop-blur-none'>
+        <DropdownMenuContent align='end' className='bg-white/50 backdrop-blur dark:bg-black/65 dark:backdrop-blur-none'>
           <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
           <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
           <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
