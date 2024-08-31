ff7037c08f17950ca23b610ebb555351c51a5049
Sulaiman Agara <airplaneboy00@gmail.com>
2024-08-31 00:20:11 -0400

    updated bio and mode toggle

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -20,7 +20,7 @@ export function ModeToggle() {
     <>
       <DropdownMenu>
         <DropdownMenuTrigger asChild className='bg-transparent dark:bg-transparent'>
-          <Button variant='outline' size='icon' className='rounded-md min-w-9 max-w-9 min-h-9 max-h-9'>
+          <Button variant='outline' size='icon' className='rounded-md min-w-9 max-w-9 min-h-9 max-h-9 z-10'>
             <SunIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
             <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
             <span className='sr-only'>Toggle theme</span>
