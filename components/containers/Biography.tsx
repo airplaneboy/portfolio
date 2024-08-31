f501945e2c53f0d91bf72ffb9afcab11d02fc595
Sulaiman Agara <airplaneboy00@gmail.com>
2024-08-31 00:11:53 -0400

    added videos for projects

--- components/containers/Biography.tsx
+++ components/containers/Biography.tsx
@@ -8,8 +8,8 @@ const Biography = () => {
       {/* <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:from-black bg-gradient-to-b from-white via-transparent to-transparent'></div> */}
       {/* <div className='absolute pointer-events-none inset-0 flex items-center justify-center bg-[linear-gradient(0deg,rgba(0,0,0,1)_0%,rgba(0,0,0,0)_15%,rgba(0,0,0,0)_70%,rgba(0,0,0,1)_90%)]' /> */}
 
-      <h1 className='text-white z-20 w-full text-center underline decoration-4 underline-offset-[4px] pt-20 relative -top-1 decoration decoration-neutral-500 uppercase'>
-        Personal Statement
+      <h1 className='z-20 w-full text-center underline decoration-4 underline-offset-[4px] pt-20 relative -top-1 decoration decoration-neutral-500 uppercase'>
+        About Me
       </h1>
 
       <div className='w-full h-full px-6 py-8 md:px-24 max-w-7xl mx-auto z-10'>
